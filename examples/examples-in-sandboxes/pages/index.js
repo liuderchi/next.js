f057b3ef65c41703174af5bfbf4f639c7377791d
liuderchi <liuderchi@gmail.com>
2021-03-06 19:32:39 +0800

    [2] add csb links
    - https://codesandbox.io/s/github/liuderchi/next.js/tree/play%2Fexamples-in-sandboxes/examples/examples-in-sandboxes

--- examples/examples-in-sandboxes/pages/index.js
+++ examples/examples-in-sandboxes/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      TODO all example sanbox links.{' '}
       <Link href="/about">
         <a>About</a>
       </Link>
